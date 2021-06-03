d354ea659a12d2ee5a81f3cc602a3e3868d00283
Patryk Trojak <patryk.j.trojak@gmail.com>
2021-06-04 01:05:50 +0200

    Remove redundant code from Fireball.cpp

--- Dungeon Plunderers/Fireball.cpp
+++ Dungeon Plunderers/Fireball.cpp
@@ -8,21 +8,6 @@ Fireball::Fireball(const sf::Vector2f& Position,
     : PlayerProjectile(Position, Velocity, Damage, Texture,
         Animation(1, 1, sf::Vector2i(53 * 2, 48 * 2), 5, 0.05f), Hitbox(Position, sf::Vector2f(35.5102f, 86.0126f), sf::Vector2f(0.f, 0.f), sf::Vector2f(-16.5265f, 20.0592)), true)
 {
-    //hitboxComponent.addHitbox(Hitbox(Position, sf::Vector2f(43.2852f, 95.9657f), sf::Vector2f(0.f, 0.f), sf::Vector2f(-26.1299f, 23.943)));
-
-    /*
-    hitboxComponent.addHitbox(Hitbox(Position, sf::Vector2f(6.05145f, 58.1949f), sf::Vector2f(15.0988f, 1.93585f)));
-    hitboxComponent.addHitbox(Hitbox(Position, sf::Vector2f(7.96776f, 54.0597f), sf::Vector2f(21.0494f, 3.85214f)));
-    hitboxComponent.addHitbox(Hitbox(Position, sf::Vector2f(8.27032f, 47.1005f), sf::Vector2f(29.0172f, 6.87787f)));
-    hitboxComponent.addHitbox(Hitbox(Position, sf::Vector2f(10.3883f, 41.9568f), sf::Vector2f(37.1866f, 10.0045f)));
-    hitboxComponent.addHitbox(Hitbox(Position, sf::Vector2f(7.96776f, 36.2079f), sf::Vector2f(47.2724f, 13.7362f)));
-    hitboxComponent.addHitbox(Hitbox(Position, sf::Vector2f(7.76605f, 27.635f), sf::Vector2f(55.1393f, 17.1654f)));
-    hitboxComponent.addHitbox(Hitbox(Position, sf::Vector2f(8.47205f, 21.7852f), sf::Vector2f(62.8044f, 19.9894f)));
-    hitboxComponent.addHitbox(Hitbox(Position, sf::Vector2f(6.05145f, 16.0364f), sf::Vector2f(71.0748f, 23.0151f)));
-    hitboxComponent.addHitbox(Hitbox(Position, sf::Vector2f(5.14374f, 12.5063f), sf::Vector2f(76.9245f, 24.6288f)));
-    hitboxComponent.addHitbox(Hitbox(Position, sf::Vector2f(4.99173f, 8.07982f), sf::Vector2f(82.0294f, 26.9718f)));
-    hitboxComponent.addHitbox(Hitbox(Position, sf::Vector2f(2.94006f, 4.01877f), sf::Vector2f(87.0634f, 28.9601f)));
-    */
     projectile.setOrigin(36, 24);
     hitboxComponent.addHitbox(Hitbox(Position, sf::Vector2f(35.5102f, 86.0126f), sf::Vector2f(0.f, 0.f), sf::Vector2f(-16.5265f, 20.0592)));
     hitboxComponent.addHitbox(Hitbox(Position, sf::Vector2f(5.98485f, 80.0806f), sf::Vector2f(0.f, 0.f), sf::Vector2f(-52.0367f, 18.0466)));
