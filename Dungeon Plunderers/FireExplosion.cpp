14b9401bd6593ad4ebd432c6af8d7e52d2061d27
Patryk Trojak <patryk.j.trojak@gmail.com>
2021-06-04 21:55:24 +0200

    Improve animation of fire explosion

--- Dungeon Plunderers/FireExplosion.cpp
+++ Dungeon Plunderers/FireExplosion.cpp
@@ -2,7 +2,7 @@
 #include "FireExplosion.h"
 
 FireExplosion::FireExplosion(const sf::Vector2f& Position, const sf::Texture& Texture)
-	: Effect(Position, Texture, 1, 1, sf::Vector2i(80, 80), 8, 0.08f, 1)
+	: Effect(Position, Texture, 1, 1, sf::Vector2i(80, 80), 7, 0.07f, 1)
 {
 
 }
