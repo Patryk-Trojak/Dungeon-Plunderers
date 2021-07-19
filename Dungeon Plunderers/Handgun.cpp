0ba67aa6ecc316d7f9513b04437827f4d0f8afc2
Patryk-Trojak <patryk.j.trojak@gmail.com>
2021-07-19 18:30:19 +0200

    Fix shooting handgun

--- Dungeon Plunderers/Handgun.cpp
+++ Dungeon Plunderers/Handgun.cpp
@@ -15,7 +15,7 @@ Handgun::~Handgun()
 
 void Handgun::attack(std::vector<std::unique_ptr<PlayerProjectile>>& Projectile, const sf::Vector2f& mousePosition)
 {
-	//if (timerOfShooting >= fireRate)
+	if (timerOfShooting >= fireRate)
 	{
 		sf::Vector2f velocityOfProjectile = sf::Vector2f(800 * cos(weapon.getRotation() * M_PI / 180) * weapon.getScale().x, 800 * sin(weapon.getRotation() * M_PI / 180) * weapon.getScale().x);
 		sf::Vector2f InitialPositionOfProjectile = sf::Vector2f((weapon.getPosition().x) + cos(weapon.getRotation() * M_PI / 180) * 45 * weapon.getScale().x + sin(weapon.getRotation() * M_PI / 180) * 15, (weapon.getPosition().y) + sin(weapon.getRotation() * M_PI / 180) * 45 * weapon.getScale().x - cos(weapon.getRotation() * M_PI / 180) * 15);
