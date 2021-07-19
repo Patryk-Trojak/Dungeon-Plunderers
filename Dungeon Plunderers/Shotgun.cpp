df937641b11970015fdf5abafc2a3f5582daf9bd
Patryk-Trojak <patryk.j.trojak@gmail.com>
2021-07-19 19:15:14 +0200

    Fix shooting shotgun

--- Dungeon Plunderers/Shotgun.cpp
+++ Dungeon Plunderers/Shotgun.cpp
@@ -19,33 +19,33 @@ Shotgun::~Shotgun()
 
 void Shotgun::attack(std::vector<std::unique_ptr<PlayerProjectile>>& Projectile, const sf::Vector2f& mousePosition)
 {
-	if (timerOfShooting > 0.4/*fireRate and currentNumberOfAmmo > 0*/)
+	if (timerOfShooting > fireRate and currentNumberOfAmmo > 0)
 	{
 		sf::Vector2f velocityOfNextProjectile = sf::Vector2f(800 * cos(weapon.getRotation() * M_PI / 180) * weapon.getScale().x, 800 * sin(weapon.getRotation() * M_PI / 180) * weapon.getScale().x);
 		sf::Vector2f PositionOfProjectiles = sf::Vector2f(weapon.getPosition().x + cos(weapon.getRotation() * M_PI / 180) * 70 * weapon.getScale().x + sin(weapon.getRotation() * M_PI / 180) * 9, weapon.getPosition().y + sin(weapon.getRotation() * M_PI / 180) * 70 * weapon.getScale().x - cos(weapon.getRotation() * M_PI / 180) * 9);
 		sf::Vector2f velocityOfFirstProjectile = velocityOfNextProjectile;
-	//	Projectile.reserve(Projectile.size() + 5);
+		Projectile.reserve(Projectile.size() + 5);
 
-		Projectile.emplace_back(std::make_unique<Bullet>(PositionOfProjectiles, velocityOfNextProjectile, 20, textureOfProjectile));
+		Projectile.emplace_back(std::make_unique<Bullet>(PositionOfProjectiles, velocityOfNextProjectile, damageOfProjectile, textureOfProjectile));
 
 		velocityOfNextProjectile.x -= sin(weapon.getRotation() * M_PI / 180) * 50;
 		velocityOfNextProjectile.y += cos(weapon.getRotation() * M_PI / 180) * 50;
-		Projectile.emplace_back(std::make_unique<Bullet>(PositionOfProjectiles, velocityOfNextProjectile, 20, textureOfProjectile));
+		Projectile.emplace_back(std::make_unique<Bullet>(PositionOfProjectiles, velocityOfNextProjectile, damageOfProjectile, textureOfProjectile));
 
 		velocityOfNextProjectile = velocityOfFirstProjectile;
 		velocityOfNextProjectile.x += sin(weapon.getRotation() * M_PI / 180) * 50;
 		velocityOfNextProjectile.y -= cos(weapon.getRotation() * M_PI / 180) * 50;
-		Projectile.emplace_back(std::make_unique<Bullet>(PositionOfProjectiles, velocityOfNextProjectile, 20, textureOfProjectile));
+		Projectile.emplace_back(std::make_unique<Bullet>(PositionOfProjectiles, velocityOfNextProjectile, damageOfProjectile, textureOfProjectile));
 
 		velocityOfNextProjectile = velocityOfFirstProjectile;
 		velocityOfNextProjectile.x -= sin(weapon.getRotation() * M_PI / 180) * 100;
 		velocityOfNextProjectile.y += cos(weapon.getRotation() * M_PI / 180) * 100;
-		Projectile.emplace_back(std::make_unique<Bullet>(PositionOfProjectiles, velocityOfNextProjectile, 20, textureOfProjectile));
+		Projectile.emplace_back(std::make_unique<Bullet>(PositionOfProjectiles, velocityOfNextProjectile, damageOfProjectile, textureOfProjectile));
 
 		velocityOfNextProjectile = velocityOfFirstProjectile;
 		velocityOfNextProjectile.x += sin(weapon.getRotation() * M_PI / 180) * 100;
 		velocityOfNextProjectile.y -= cos(weapon.getRotation() * M_PI / 180) * 100;
-		Projectile.emplace_back(std::make_unique<Bullet>(PositionOfProjectiles, velocityOfNextProjectile, 20, textureOfProjectile));
+		Projectile.emplace_back(std::make_unique<Bullet>(PositionOfProjectiles, velocityOfNextProjectile, damageOfProjectile, textureOfProjectile));
 		timerOfShooting = 0;
 		currentNumberOfAmmo--;
 	}
